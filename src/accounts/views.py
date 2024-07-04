1d844fcf462174ef9c3e9ef3d599743e50c8d8a5
Harry <hjwp2@cantab.net>
2024-07-04 06:19:03 +0100

    view now sends mail with correct from/to/subject. --ch17l006--

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import redirect
 
 
 def send_login_email(request):
+    email = request.POST["email"]
+    send_mail(
+        "Your login link for Superlists", "body text tbc", "noreply@superlists", [email]
+    )
     return redirect("/")
