1c8fe1efbd97da72652c8b08973592ffafc694ab
Harry <hjwp2@cantab.net>
2024-07-04 06:19:08 +0100

    placeholder get_user method. --ch17l031--

--- src/accounts/authentication.py
+++ src/accounts/authentication.py
@@ -10,3 +10,6 @@ class PasswordlessAuthenticationBackend(object):
             return User.objects.create(email=token.email)
         except Token.DoesNotExist:
             return None
+
+    def get_user(self, email):
+        pass
