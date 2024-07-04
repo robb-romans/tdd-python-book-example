66be868bef065d1a249f9df443f0e0acfb3d47ef
Harry <hjwp2@cantab.net>
2024-07-04 06:19:05 +0100

    add success message. --ch17l014--

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -6,5 +7,8 @@ def send_login_email(request):
     email = request.POST["email"]
     send_mail(
         "Your login link for Superlists", "body text tbc", "noreply@superlists", [email]
+    )
+    messages.success(
+        request, "Check your email, we've sent you a link you can use to log in."
     )
     return redirect("/")
