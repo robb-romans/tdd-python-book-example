d6fbccaa41b9aaeb616a2424584646e642f9bd8a
Harry <hjwp2@cantab.net>
2024-07-04 06:19:06 +0100

    cheat with email body for now. --ch17l016--

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import redirect
 def send_login_email(request):
     email = request.POST["email"]
     send_mail(
-        "Your login link for Superlists", "body text tbc", "noreply@superlists", [email]
+        "Your login link for Superlists",
+        "Use this link to log in",
+        "noreply@superlists",
+        [email],
     )
     messages.success(
         request, "Check your email, we've sent you a link you can use to log in."
