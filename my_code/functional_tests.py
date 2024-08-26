7a5d16cf3e18f9469744db418ba9331845602094
Robb Romans <rromans@squarespace.com>
2024-08-26 16:06:19 -0500

    ch1-rr-initial-commit

---
+++ my_code/functional_tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_open(self):
+        # Test bare Django server
+        self.browser.get("http://localhost:8000")
+        self.assertIn("Congratulations!", self.browser.title)
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
