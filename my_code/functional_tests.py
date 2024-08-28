453b3d1921504942245e5346c8c2db8e999cd6ed
Robb Romans <rromans@squarespace.com>
2024-08-28 17:13:27 -0500

    rr ch 2 changes

--- my_code/functional_tests.py
+++ my_code/functional_tests.py
@@ -9,10 +9,27 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_open(self):
-        # Test bare Django server
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
         self.browser.get("http://localhost:8000")
-        self.assertIn("Congratulations!", self.browser.title)
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title, f"Browser title was {self.browser.title}")
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+
+# She types "Buy peacock feathers" into a text box
+# (Edith's hobby is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item.
+# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Satisfied, she goes back to sleep
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
